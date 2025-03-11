868e87f2d6f01048361db970983ef712e6ce0da6
duki994 <duki994@gmail.com>
2025-03-12 00:27:25 +0100

    tryfrom

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::{io::Read, net::TcpListener};
+use crate::http::{request, Request};
 
 pub struct Server {
     addr: String,
@@ -17,7 +18,25 @@ impl Server {
 
         loop {
             match listener.accept() {
-                
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+
+                    // what if buffer is too small? (avoid handling this; for course learning purposes)
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => todo!(),
+                                Err(e) => println!("Failed to parse the request: {}", e),
+                            }
+                        },
+                        Err(_) => todo!(),
+                    }
+
+
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
             }
         }
     }
